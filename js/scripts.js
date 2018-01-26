30326ac9e7435676bdcdf2d64519fbe02ff3934b
johnnymayer <johnnymayer@gmail.com>
2018-01-26 11:50:12 -0800

    More JS scripting, debug problem with undefined returns check lines 47 in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,35 @@ function Pizza(size, topping1, topping2) {
   this.topping2 = topping2;
 }
 
-Pizza.prototype.pizzaName = function () {
-  return "Your pizza: " + this.size + ", " + this.topping1 + ", " + this.topping2 + " is ready.";
+Pizza.prototype.pizzaName = function() {
+  return "Your pizza a: " + this.size + ", with " + this.topping1 + ", and " + this.topping2 + " is ready.";
 };
 
 var pricePizza = function(inputtedSizeChoice, inputtedTopping1, inputtedTopping2) {
-  var price = 4.99;
+  var price = 5;
+  var priceToppings = 0;
   if (inputtedSizeChoice === "5") {
-    price = price + 5;
+    priceToppings = priceToppings + 5;
   } else if (inputtedSizeChoice === "6") {
-    price = price + 6;
+    priceToppings = priceToppings + 6;
   } else {
-    price = price + 12;
+    priceToppings = priceToppings + 12;
   }
-  if (inputtedTopping1 === "1") {
-    price = price + 1;
+  if (inputtedTopping1 === "4") {
+    priceToppings = priceToppings + 1;
+  } else if (inputtedTopping1 === "5"){
+    priceToppings = priceToppings + 2;
   } else {
-    price = price + 2;
+    priceToppings = priceToppings + 6;
   }
-  if (inputtedTopping2 === "1") {
-    price = price + 1;
+  if (inputtedTopping2 === "4") {
+    priceToppings = priceToppings + 1;
+  } else if (inputtedTopping2 === "5"){
+    priceToppings = priceToppings + 2;
   } else {
-    price = price + 2;
+    priceToppings = priceToppings + 6;
   }
-  return price;
+  return "It will cost $" + price + priceToppings;
 };
 
 //UI
@@ -39,5 +44,6 @@ $(document).ready(function() {
       var inputtedTopping1 = $("input#topping1").val();
       var inputtedTopping2 = $("input#topping2").val();
       var newPizza = new Pizza(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
+      $("ul#pizzaInfo").append("<span class='pizzaTitle'>" + newPizza.pizzaName() + "<span>");
     });
   });
