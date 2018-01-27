648b454cc6142919031467830806298a3927f5af
Johnny Mayer <johnnymayer@gmail.com>
2018-01-26 16:30:00 -0800

    pizzaPrice debug still in progress.

--- js/scripts.js
+++ js/scripts.js
@@ -20,25 +20,26 @@ var pizzaPrice = function(inputtedSizeChoice, inputtedTopping1, inputtedTopping2
   }
   if (inputtedTopping1 === "Cheese") {
     price = price + 1;
-  } else if (inputtedTopping1 === "Tomatoes" || "Peppers" || "Soyrizo") {
+  } else if (inputtedTopping1 === "Tomatoes" || inputtedTopping1 === "Peppers" || inputtedTopping1 === "Soyrizo") {
     price = price + 2;
   } else {
     price = price + 3;
   }
   if (inputtedTopping2 === "Cheese") {
     price = price + 1;
-  } else if (inputtedTopping2 === "Tomatoes" || "Peppers" || "Soyrizo") {
+  } else if (inputtedTopping1 === "Tomatoes" || inputtedTopping1 === "Peppers" || inputtedTopping1 === "Soyrizo") {
     price = price + 2;
   } else {
     price = price + 3;
   }
+  console.log(price);
   return price;
 };
 
-var determinePrice = function(pizzaPrice) {
- if (price <= 3) {
+var determinePrice = function(price) {
+ if (price <= 4) {
    $("#price10").text($("ul#pizzaInfo").append("<li><span class='price'>$10</span></li>"));
- } else if (price > 4 && price < 7 ) {
+ } else if (price > 5 && price < 6 ) {
    $("#price20").text($("ul#pizzaInfo").append("<li><span class='price'>$20</span></li>"));
  } else {
    $("#price30").text($("ul#pizzaInfo").append("<li><span class='price'>$50</span></li>"));
@@ -55,11 +56,13 @@ $(document).ready(function() {
     var inputtedTopping2 = $("#topping2").val();
 
     var newPizza = new Pizza(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
-    var pizzaInfo = "Your pizza a: " + this.size + ", with " + this.topping1 + ", and " + this.topping2 + " is ready.";
-    $("ul#pizzaInfo").append("<span class='pizzaTitle'>" + newPizza.pizzaName() + "<span>");
-    var price = pizzaPrice(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
 
-    console.log(price);
+    var pizzaInfo = "Your pizza a: " + this.size + ", with " + this.topping1 + ", and " + this.topping2 + " is ready.";
+
+    var priceDetail = pizzaPrice(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
+
+    $("ul#pizzaInfo").append("<span class='pizzaTitle'>" + newPizza.pizzaName() + "<span>");
+    console.log(pizzaInfo);
     determinePrice(pizzaPrice);
   });
 });
