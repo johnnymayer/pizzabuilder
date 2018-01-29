f269f40fa6ae0dd3450b281fd2effc0acfdd8aab
Guest User <Guest@Epicodus-7B.local>
2018-01-29 13:09:02 -0800

    Add JS functionality, new prototype, include pricing scheme in pizzaPrice function.

--- js/scripts.js
+++ js/scripts.js
@@ -9,40 +9,28 @@ Pizza.prototype.pizzaName = function() {
   return "Your pizza : " + this.size + ", with " + this.topping1 + ", and " + this.topping2 + " is ready.";
 };
 
-var pizzaPrice = function(inputtedSizeChoice, inputtedTopping1, inputtedTopping2) {
+Pizza.prototype.pizzaPrice = function() {
   var price = 0;
-  if (inputtedSizeChoice === "Small") {
-    price = price + 1;
-  } else if (inputtedSizeChoice === "Large") {
-    price = price + 2;
+  if (this.size === "Small") {
+    price = 10;
+  } else if (this.size === "Large") {
+    price = 15;
   } else {
-    price = price + 3;
+    price = 30;
   }
-  if (inputtedTopping1 === "Cheese") {
+  if (this.topping1 === "Cheese") {
     price = price + 1;
-  } else if (inputtedTopping1 === "Tomatoes" || inputtedTopping1 === "Peppers" || inputtedTopping1 === "Soyrizo") {
+  } else if (this.topping1 === "Tomatoes" || this.topping1 === "Peppers" || this.topping1 === "Soyrizo") {
     price = price + 2;
   } else {
-    price = price + 3;
+    price = price;
   }
-  if (inputtedTopping2 === "Cheese") {
+  if (this.topping2 === "Cheese") {
     price = price + 1;
-  } else if (inputtedTopping1 === "Tomatoes" || inputtedTopping1 === "Peppers" || inputtedTopping1 === "Soyrizo") {
+  } else if (this.topping2 === "Tomatoes" || this.topping2 === "Peppers" || this.topping2 === "Soyrizo") {
     price = price + 2;
-  } else {
-    price = price + 3;
   }
   return price;
-};
-
-var determinePrice = function(price) {
- if (price <= 4) {
-   $("#price10").text($("ul#pizzaInfo").append("<li><span class='price'>$10</span></li>"));
- } else if (price > 5 && price < 6 ) {
-   $("#price20").text($("ul#pizzaInfo").append("<li><span class='price'>$20</span></li>"));
- } else {
-   $("#price30").text($("ul#pizzaInfo").append("<li><span class='price'>$50</span></li>"));
- }
 };
 
 //UI
@@ -54,13 +42,15 @@ $(document).ready(function() {
     var inputtedTopping1 = $("#topping1").val();
     var inputtedTopping2 = $("#topping2").val();
 
-    var newPizza = new Pizza(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
 
+    var newPizza = new Pizza(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
+    console.log(newPizza);
     var pizzaInfo = "Your pizza a: " + this.size + ", with " + this.topping1 + ", and " + this.topping2 + " is ready.";
 
-    var priceDetail = pizzaPrice(inputtedSizeChoice, inputtedTopping1, inputtedTopping2);
-
-    $("ul#pizzaInfo").append("<span class='pizzaTitle'>" + newPizza.pizzaName() + "<span>");
+    var priceDetail = newPizza.pizzaPrice();
+    alert(priceDetail);
+    $("ul#pizzaInfo").empty();
+    $("ul#pizzaInfo").append("<span class='pizzaTitle'>" + newPizza.pizzaName() + " $" + newPizza.pizzaPrice() + "<span>");
     determinePrice(pizzaPrice);
   });
 });
